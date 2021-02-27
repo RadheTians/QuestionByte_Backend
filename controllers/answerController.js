0e11dcb81294f0e9d4628c6e5b848fb3ab7d26f0
RadheTians <radheramantiwari87@gmail.com>
2021-02-27 16:12:22 +0530

    Answer list

---
+++ controllers/answerController.js
@@ -0,0 +1,37 @@
+const Answer = require('./../models/answerModel');
+const catchAsync = require('./../utils/catchAsync');
+
+exports.askQuestion = catchAsync(async (req, res, next) => {
+
+    const answer = await Answer.create(req.body);
+    if (answer) {
+        res.status(200).json({
+            status: 'success',
+            data: answer 
+        });
+    } else {
+        res.status(200).send({ 
+            status: 'fail',
+            message: 'Invalid Answer Data.'
+         });
+    }
+});
+
+
+exports.getAllAnswers = catchAsync(async (req, res, next) => {
+
+    console.log(req.body)
+    const question = await Question.find({ questionId: req.body.id });
+    if (question) {
+        res.status(200).json({
+            status: 'success',
+            data: question 
+        });
+    } else {
+        res.status(200).send({ 
+            status: 'fail',
+            message: 'Invalid Question Data.'
+         });
+    }
+});
+
---
+++ models/answerModel.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const answerSchema = new mongoose.Schema({
+    questionId :{
+        type: String,
+        required: true,
+    },
+    userId :{
+        type: String,
+        required: true
+    },
+    userName :{
+        type: String,
+        required: true
+    },
+    answer: { 
+        type: String, 
+        required: true,
+        unique: true
+    },
+    answeredTime: {
+        type: Date,
+        default: Date.now
+    },
+    
+});
+
+module.exports = mongoose.model('Answer', answerSchema);
+
