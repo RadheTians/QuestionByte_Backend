6bdbdbd1fba6ea85be37b442f34a7359726c1cc1
RadheTians <radheramantiwari87@gmail.com>
2021-03-03 02:35:26 +0530

    updated code

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -20,20 +20,11 @@ exports.askQuestion = catchAsync(async (req, res, next) => {
 exports.upAndDownVoteQuestion = catchAsync(async (req, res, next) => {
 
     const question = await Question.findById(req.body.id);
-    const userId = req.body.userId;
-    question.votes.forEach(element => {
-        if(userId === element){
-            res.status(400).send({ 
-                status: 'fail',
-                message: 'You have already voted!!!'
-             });
-        }
-    });
-    question.votes.push(userId);
+    question.votes.push(req.body.userId);
     await question.save();
     res.status(200).json({
         status: 'success',
-        data: 'Your vote has been saved.'
+        data: 'Your vote has been recorded.'
     });
     
 });
