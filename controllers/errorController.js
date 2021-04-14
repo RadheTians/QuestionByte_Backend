764a99340a8c9271b5d182810a0044009a14c90c
RadheTians <radheramantiwari87@gmail.com>
2021-04-14 15:48:11 +0530

    new update

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -23,8 +23,8 @@ const handleDuplicateFieldsDB = (err) => {
 const handleValidationErrorDB = (err) => {
   const errors = Object.values(err.errors).map((el) => el.message);
 
-  const message = `Invalid input data. ${errors.join('. ')}`;
-  return new ErrorUtil(message, 400);
+  const message = errors;
+  return new ErrorUtil(message, 200);
 };
 
 const handleJWTError = () =>
